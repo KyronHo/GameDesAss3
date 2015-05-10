455d25bf73fab824514cf4bdf19691d06ab9b939
KyronHo <KyronCHo@gmail.com>
2015-05-10 21:52:14 +1000

    Timer transition
    added for roman intro

---
+++ Assets/Scripts/IntroTimer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroTimer : MonoBehaviour {
+
+	public int level;
+	public float timer;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0) {
+			if(level ==0){
+			Application.LoadLevel("Roman_Level");
+			}
+		}
+	}
+}
