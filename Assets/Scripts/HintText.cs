c79bff8128c913905d991a78414130033fefac7a
KyronHo <KyronCHo@gmail.com>
2015-05-26 17:52:38 +1000

    Hint placeholder

---
+++ Assets/Scripts/HintText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HintText : MonoBehaviour {
+	
+	public int level;
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Button>().onClick.AddListener(ShowHint);
+	}
+	
+	void ShowHint() {
+		foreach (Image i in GetComponentsInChildren<Image>()) {
+			i.enabled = true;
+		}
+	}
+}
