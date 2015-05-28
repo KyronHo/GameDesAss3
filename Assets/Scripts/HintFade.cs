17e56079793474e781e83e9a7a83fe5afa2957d7
KyronHo <KyronCHo@gmail.com>
2015-05-28 18:11:09 +1000

    Added hint text

---
+++ Assets/Scripts/HintFade.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HintFade : MonoBehaviour {
+
+	private float timer;
+	public Color temp;
+	// Use this for initialization
+	void Start () {
+		timer = 5f;
+		temp = gameObject.GetComponent<Image>().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.GetComponent<Image>().enabled == true) {
+			timer -= Time.deltaTime;
+			if(timer < 0){
+				temp.a -= .01f;
+				gameObject.GetComponent<Image>().color = temp;}
+		}
+	}
+}
--- Assets/Scripts/HintText.cs
+++ Assets/Scripts/HintText.cs
@@ -2,16 +2,16 @@
 using UnityEngine.UI;
 
 public class HintText : MonoBehaviour {
-	
-	public int level;
+
 	// Use this for initialization
 	void Start () {
 		this.GetComponent<Button>().onClick.AddListener(ShowHint);
 	}
 	
 	void ShowHint() {
-		foreach (Image i in GetComponentsInChildren<Image>()) {
+		foreach (Image i in GetComponentsInParent<Image>()) {
 			i.enabled = true;
 		}
+		Destroy (transform.gameObject);
 	}
 }
